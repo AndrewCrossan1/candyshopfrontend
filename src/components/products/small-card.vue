e5f244b14625dbf4861bec48fd7bb019ea4ddaed
AndrewCrossan1 <andrew.crossan23@outlook.com>
2023-01-12 01:09:18 +0000

    View option now wired in, design tweaks also applied

--- src/components/products/small-card.vue
+++ src/components/products/small-card.vue
@@ -1,30 +1,67 @@
 <template>
-  <div>
-    <b-card class="border-1">
-      <b-card-img
-          :src="product.image"
-          :alt="product.name"
-          class="img-fluid"
-          style="max-width: 100%; max-height: 150px;"
-      />
-      <b-card-body>
-        <b-card-title class="text-sm text-center">
-          {{ product.name }}
-        </b-card-title>
-        <b-card-text class="text-center">
-          {{ product.price }}
-        </b-card-text>
-        <b-card-text>
-          <b-rating color="#ff69b4" :value="product.rating" readonly/>
-        </b-card-text>
-      </b-card-body>
-      <b-card-footer>
-        <b-button variant="outline-hot-pink" class="w-100">
-          Add to cart
-        </b-button>
-      </b-card-footer>
-    </b-card>
-  </div>
+    <div v-if="view === 'grid' || view === null" class="col-md-6 col-12 col-sm-6 col-lg-4 col-xl-3 mb-2 mb-xl-3 mb-lg-2 mb-md-1 d-flex align-items-stretch">
+      <b-card class="border-1">
+        <b-card-img
+            :src="product.Image"
+            :alt="product.Name"
+            class="img-fluid"
+            style="max-width: 100%;"
+        />
+        <b-card-body class="pt-2 px-0">
+          <b-card-title class="text-sm text-center ">
+            {{ product.Name}}
+          </b-card-title>
+          <b-card-text class="text-center">
+            {{ product.Price | currency }}
+            <b-rating color="#ff69b4" :value="product.Prop_Rating" readonly/>
+          </b-card-text>
+        </b-card-body>
+        <b-card-footer>
+          <b-button variant="outline-hot-pink" class="mt-auto w-100">
+            Add to cart
+          </b-button>
+        </b-card-footer>
+      </b-card>
+    </div>
+    <div v-else-if="view === 'list'" class="col-12 my-2">
+      <b-card class="overflow-hidden">
+        <b-row no-gutters>
+          <div class="col-xl-4">
+            <b-card-img
+                :src="product.Image"
+                :alt="product.Name"
+                class="img-fluid"
+                style="max-width: 100%;"
+            />
+          </div>
+          <div class="col-xl-8">
+            <b-card-body class="pt-2 px-0 d-flex align-items-center justify-content-between">
+              <b-card-title class="text-sm text-center">
+                {{ product.Name}}
+                <br/>
+                <br/>
+                <b-link :to="{ name: 'productsByCategory'}">
+                  {{ product.category_name}}
+                </b-link>
+              </b-card-title>
+              <b-card-text class="text-center">
+                {{ product.Price | currency }}
+                <b-rating color="#ff69b4" class="mt-2" :value="product.Prop_Rating" readonly/>
+                ({{ product.Prop_Rating }}/5)
+              </b-card-text>
+            </b-card-body>
+            <b-card-body class="pt-2 px-0 d-flex align-items-center justify-content-between">
+              {{ product.Description }}
+            </b-card-body>
+            <b-card-footer>
+              <b-button variant="outline-hot-pink" class="mt-auto w-100">
+                Add to cart
+              </b-button>
+            </b-card-footer>
+          </div>
+        </b-row>
+      </b-card>
+    </div>
 </template>
 
 <script>
@@ -34,6 +71,24 @@ export default {
     product: {
       type: Object,
       required: true,
+    },
+    view: {
+      type: String,
+      default: null,
+    },
+  },
+  filters: {
+    currency(value) {
+      let formatter = Intl.NumberFormat('en-GB', {
+        style: 'currency',
+        currency: 'GBP',
+      });
+      return formatter.format(value);
+    }
+  },
+  data() {
+    return {
+      price: "",
     }
   }
 }
