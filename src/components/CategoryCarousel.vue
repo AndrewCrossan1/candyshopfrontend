fb893a12834011effc55eaf9fa3f8b04f8572619
Andrew Crossan <andrew.crossan23@outlook.com>
2023-01-18 01:21:17 +0000

    Added category carousel for shop page

---
+++ src/components/CategoryCarousel.vue
@@ -0,0 +1,35 @@
+<template>
+  <carousel :perPage="5" :mouse-drag="true" class="mb-3" :autoplay="true" :touch-drag="true" :paginationEnabled="false">
+    <slide v-for="category in categories" :key="category.CategoryID" class="category-pill">
+      <router-link :to="{name: 'Products by category', params: {name: category.get_url}}">{{ category.Name}}</router-link>
+    </slide>
+  </carousel>
+</template>
+
+<script>
+
+import { Carousel, Slide } from 'vue-carousel';
+
+export default {
+  name: "CategoryCarousel",
+  props: {
+    categories: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      category: null
+    };
+  },
+  components: {
+    Carousel,
+    Slide
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
