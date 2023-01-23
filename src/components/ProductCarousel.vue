134f0b848650db02daa2fb2d221238e738e6ed9a
Andrew Crossan <andrew.crossan23@outlook.com>
2023-01-23 20:46:11 +0000

    Added ProductCarousel.vue for recently viewed products

---
+++ src/components/ProductCarousel.vue
@@ -0,0 +1,34 @@
+<template>
+  <carousel :perPage="5" :mouse-drag="true" class="mb-3" :autoplay="true" :touch-drag="true" :paginationEnabled="false">
+    <slide v-for="product in products" :key="product.ProductID" class="category-pill">
+      <b-link class="category-link" :to="{name: 'Product', params: {name: product.get_url}}">{{ product.Name}}</b-link>
+    </slide>
+  </carousel>
+</template>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel';
+
+export default {
+  name: "CategoryCarousel",
+  props: {
+    products: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      category: null
+    };
+  },
+  components: {
+    Carousel,
+    Slide
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
